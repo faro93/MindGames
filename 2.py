41c3100cc7ef526a466cd4408b592b2d3cbe914d
Fabrice <58191361+faro93@users.noreply.github.com>
2020-06-27 01:24:36 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,31 @@
+# coding utf-8
+import time
+
+class fib():
+    def __init__(self, max):
+        print(f'Calcul des n termes de Fibonacci inférieurs à {max}')
+        self.first_term = int()
+        self.second_term = int()
+        self.next_term = int()
+        self.sum = int()
+        self.result = int()
+
+        c = 2
+        while self.next_term <= max:
+            if c > 2:
+                self.first_term, self.second_term = self.second_term, self.next_term
+                self.next_term = self.first_term + self.second_term
+            else:
+                self.first_term, self.second_term = 1, 2
+                self.next_term = self.first_term + self.second_term
+            c += 1
+            if self.second_term%2 == 0:
+                print(f'{self.sum}+{self.second_term}=', end='')
+                self.sum += self.second_term
+                print(f'{self.sum}')
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    f = fib(4_000_000)
+    print(f'Durée d\'exécution = {time.time()-start_time}s ')
