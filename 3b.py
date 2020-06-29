142a1653f3acf26d8b4f7877ccd71c56818cee53
faro93 <fgithub@famille-rousselot.net>
2020-06-29 20:40:13 +0200

    3b.py

---
+++ 3b.py
@@ -0,0 +1,62 @@
+#coding utf-8
+import time
+
+def main(n):
+    print(f'Recherche du plus grand facteur premier pour {n}')
+    premiers = list()
+    facteurs = list()
+    quotient = int()
+    indexpremiers = 0
+    divisé = n
+    start_time = time.time()
+
+    premiers = PremierSuivant(premiers)
+    while quotient != 1:
+        if divisé%premiers[indexpremiers] == 0:
+            quotient = divisé//premiers[indexpremiers]
+            divisé = quotient
+            facteurs.append(premiers[indexpremiers])
+        else:
+            premiers = PremierSuivant(premiers)
+            indexpremiers += 1
+
+    # for n in range(1000):
+    #     premiers = PremierSuivant(premiers)
+    #     print(f'premiers={premiers} ')
+
+    print(f'Le produit de facteurs premiers de {n} est : ',end='')
+    for i in facteurs:
+        print(f'{i}', end='')
+        if facteurs.index(i) != len(facteurs)-1:
+            print(f'x', end='')
+    print('')
+    print(f'Le plus grand facteur premier de {n} est {facteurs[-1]}')
+    print(f'Durée d\'exécution : {time.time()-start_time}s.')
+
+def PremierSuivant(lp):
+    # print(f'lp={lp} ')
+    if len(lp) == 0:
+        compteur = 2
+        lp.append(compteur)
+    else:
+        compteur = lp[-1]+1
+        indexlp = 0
+        trouvé = False
+        while trouvé == False and indexlp < len(lp):
+            if compteur%lp[indexlp] == 0:
+                compteur += 1
+                indexlp = 0
+            else:
+                if indexlp == len(lp)-1:
+                    lp.append(compteur)
+                    trouvé = True
+                else:
+                    indexlp +=1
+    return lp
+
+if __name__ == "__main__":
+    main(12)
+    print()
+    main(13195)
+    print()
+    main(600851475143)
