f5f09fb9f2ff26a46712f14ceac9e9807a3ab3e4
ROUSSELOT Fabrice DTSI/DERS <FIOR7513@orange.com>
2020-07-07 17:46:08 +0200

    Résultat5.py ok

--- 5.py
+++ 5.py
@@ -3,13 +3,32 @@
 import time
 
 class Ediv():
-    def __init__(self, max):
-        self.max = max
+    def __init__(self, mdiv):
+        self.mdiv = mdiv
+        print(f'Recherche du plus petit entier  divisible sans reste par les valeurs de {self.mdiv} à 1.')
 
     def Recherche(self):
-        pass
+        trouvé = False
+        compteur = self.mdiv
+        div = self.mdiv
+        while not trouvé and div != 0:
+            # print(f'compteur={compteur}, div={div}, mod={compteur%div}')
+            if compteur%div == 0:
+                div -= 1
+                if div == 0:
+                    trouvé = True
+                    print(f'Résultat={compteur}')
+            else:
+                div = self.mdiv
+                compteur += self.mdiv
+        return True
 
-if __name__ == "__maine__":
+if __name__ == "__main__":
+    start_time = time.time()
+    i = Ediv(10)
+    r = i.Recherche()
+    print(f'Exécution en {time.time()-start_time}s.')
     start_time = time.time()
     i = Ediv(20)
+    r = i.Recherche()
     print(f'Exécution en {time.time()-start_time}s.')
