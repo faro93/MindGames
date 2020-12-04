cdf9447b2273fa3e604975fe69543fa8b7183ac8
ROUSSELOT Fabrice DTSI/DERS <FIOR7513@orange.com>
2020-12-04 13:39:21 +0100

    202012041338-e18b

--- 018.py
+++ 018.py
@@ -42,6 +42,24 @@ t1=[[3],
     [2, 4, 6],
     [8, 5, 9, 3]]
 
+t2 = [
+[75],
+[95, 64],
+[17, 47, 82],
+[18, 35, 87, 10],
+[20,  4, 82, 47, 65],
+[19,  1, 23, 75,  3, 34],
+[88,  2, 77, 73,  7, 63, 67],
+[99, 65,  4, 28,  6, 16, 70, 92],
+[41, 41, 26, 56, 83, 40, 80, 70, 33],
+[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+[63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+[ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]
+]
+
 class Dijkstra():
     def __init__(self, m):
         self.m = m
@@ -176,12 +194,13 @@ class GraphicConvert():
 
 if __name__ == '__main__':
     starttime = time.time()
-    print(f'triangle:')
-    for l in t1:
+    for t in [t1, t2]:
+        print(f'triangle:')
+        for l in t:
             print(f'{l}')
-    gc = GraphicConvert()
-    m = gc.Triangle2Matrix(t1)
-    # print(m)
-    D = Dijkstra(m)
-    D.LightWeight(0)
+        gc = GraphicConvert()
+        m = gc.Triangle2Matrix(t)
+        # print(m)
+        D = Dijkstra(m)
+        D.LightWeight(0)
     print(f'Durée d\'exécution : {time.time()-starttime}s')
